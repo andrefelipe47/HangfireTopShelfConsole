837fe44fa0bfdb15eda08f5dfd418143bfceae22
André Martins <andre.felipe47@hotmail.com>
2021-10-01 13:47:02 -0300

    HangFire em Console

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebApp.Start<Startup>("http://localhost:8888"))
+            {
+                Console.WriteLine("Server started... press ENTER to shut down");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Hangfire;
+using Microsoft.Owin;
+using Owin;
+using System;
+
+[assembly: OwinStartup(typeof(ConsoleApp1.Startup))]
+
+namespace ConsoleApp1
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseErrorPage();
+
+            GlobalConfiguration.Configuration.UseSqlServerStorage("Server=127.0.0.1;Database=hangfire;User Id=hangfire;Password=123456;");
+
+            app.UseHangfireServer();
+            app.UseHangfireDashboard("/hangfire");
+
+            RecurringJob.AddOrUpdate(
+    () => Console.WriteLine("Recurring!"),
+    Cron.Minutely);
+        }
+    }
+}
